d9dde9631d9bfc7c67fe6985af85434bf49fd192
Kristen M <kristenmayorga@gmail.com>
2020-08-09 20:21:01 -0400

    team profile working

--- Index.js
+++ Index.js
@@ -1,2 +1,192 @@
-const inquirer = require("inquirer");
+
 const fs = require("fs");
+
+const inquirer = require("inquirer");
+const path = require("path");
+
+
+
+const Employee = require("./lib/Employee");
+const Engineer  = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+
+const generatePage= require("./src/generateProfile");
+
+const teamArray = [];
+
+
+// Starting prompt 
+function startingPrompt() {
+  inquirer
+    .prompt([
+      {
+        message:
+       "Welcome, please write your team name",
+        name: "team",
+        default: "My Team"
+      },
+    ])
+    .then(function (data) {
+      const teamName = data.teamname;
+    //   teamArray.push(teamName);
+      addManager();
+    });
+}
+
+
+
+// Add Manager 
+function addManager() {
+    inquirer.prompt([
+        {
+            message: "What is your team manager's name?",
+            name: "name",
+            default: "Kristen"
+        },
+        {
+            message: "What is your team manager's email address?",
+            name: "email",
+            default :"test@test.com"
+        },
+
+        {
+            type: "number",
+            message: "What is your team manager's office number?",
+            name: "officeNumber",
+            default: "555-555-555"
+        },
+    ])
+
+        .then(function (data) {
+            const name = data.name
+            const id = 1
+            const email = data.email
+            const officeNumber = data.officeNumber
+            const teamMember = new Manager(name, id, email, officeNumber)
+            teamArray.push(teamMember)
+            addTeamMembers();
+        });
+    }
+
+// Add Team Members
+function addTeamMembers() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Would you like to add more team members?",
+        choices: [
+          "Yes, add an engineer",
+          "Yes, add an intern",
+          "No, my team is complete",
+        ],
+        name: "addMemberData",
+      },
+    ])
+
+    .then(function (data) {
+      switch (data.addMemberData) {
+        case "Yes, add an engineer":
+          addEngineer();
+          break;
+
+        case "Yes, add an intern":
+          addIntern();
+          break;
+        case "No, my team is complete":
+        console.log(teamArray);
+         const html =  generatePage(teamArray);
+         console.log(html);
+         fs.writeFile(
+           "./dist/index.html",
+           html,
+           (err) => {
+             if (err) throw err;
+
+             console.log(
+               "Team complete! Check out index.html to see the output!"
+             );
+           }
+         );
+          break;
+      }
+    });
+}
+
+//Add Engineer 
+function addEngineer() {
+    inquirer.prompt([
+        {
+            message: "What is this engineer's name?",
+            name: "name",
+            default : "Test"
+        },
+        {
+            message: "What is this engineer's email address?",
+            name: "email",
+            default: "test2@test.com"
+        },
+        {
+            message: "What is this engineer's Github profile?",
+            name: "github",
+            default : "Test"
+        }
+    ])
+
+        .then(function (data) {
+            const name = data.name
+            const id = teamArray.length + 1
+            const email = data.email
+            const github = data.github
+            const teamMember = new Engineer(name, id, email, github)
+            teamArray.push(teamMember)
+            addTeamMembers()
+        });
+
+};
+
+//Add Intern 
+function addIntern() {
+    inquirer.prompt([
+        {
+            message: "What is this intern's name?",
+            name: "name",
+            default: "Intern"
+        },
+        {
+            message: "What is this intern's email address?",
+            name: "email",
+            default: "intern@test.com"
+        },
+        {
+            message: "What is this intern's school?",
+            name: "school",
+            default : "UCF"
+        }
+    ])
+
+        .then(function (data) {
+            const name = data.name
+            const id = teamArray.length + 1
+            const email = data.email
+            const school = data.school
+            const teamMember = new Intern(name, id, email, school)
+            teamArray.push(teamMember)
+            addTeamMembers()
+        });
+
+};
+
+
+
+
+
+
+// });
+
+ 
+
+
+startingPrompt();
+
---
+++ src/generateProfile.js
@@ -0,0 +1,114 @@
+
+const renderEmployees = Employees => {
+
+
+const renderManager = Manager => {
+    return `
+    <div class="card mx-auto" style="width: 18rem">
+    <h5 class="card-header">${Manager.getRole()}</h5>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">${Manager.name}</li>
+        <li class="list-group-item">${Manager.email}</li>
+        <li class="list-group-item">${Manager.getOfficeNumber()}</li>
+    </ul>
+</div>
+
+    `;
+}
+
+const renderEngineer = Engineer => {
+  return `
+    <div class="card mx-auto" style="width: 18rem">
+    <h5 class="card-header">${Engineer.getRole()}</h5>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">${Engineer.name}</li>
+        <li class="list-group-item">${Engineer.email}</li>
+        <li class="list-group-item">${Engineer.getGithub()}</li>
+    </ul>
+</div>
+
+    `;
+};
+
+
+const renderIntern = Intern => {
+  return `
+    <div class="card mx-auto" style="width: 18rem">
+    <h5 class="card-header">${Intern.getRole()}</h5>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">${Intern.name}</li>
+        <li class="list-group-item">${Intern.email}</li>
+        <li class="list-group-item">${Intern.getSchool()}</li>
+    </ul>
+</div>
+
+    `;
+};
+
+ const html = [];
+ html.push(
+   Employees
+     .filter((employee) => employee.getRole() === "Manager")
+     .map((manager) => renderManager(manager))
+ );
+ html.push(
+   Employees
+   .filter((employee) => employee.getRole() === "Engineer")
+     .map((engineer) => renderEngineer(engineer))
+     .join("")
+ );
+ html.push(
+   Employees
+   .filter((employee) => employee.getRole() === "Intern")
+     .map((intern) => renderIntern(intern))
+     .join("")
+ );
+ console.log(html);
+ return html.join("");
+
+
+}
+
+generatePage = teamArray => {
+  
+ 
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>My Team</title>
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="style.css" />
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+  </head>
+
+  <body>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12 jumbotron mb-3 team-heading navbar-light" style="background-color: #e3f2fd;">
+          <h1 class="text-center">My Team</h1>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="team-area col-12 d-flex justify-content-center">
+          ${renderEmployees(teamArray)}
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+    `;
+}
+
+
+module.exports = generatePage;
